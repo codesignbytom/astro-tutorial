30622e5921a163798f17033c33b5ec64c9c798b9
Tom Graafmans <graafmans.tom@gmail.com>
2023-11-05 00:19:09 +0100

    Add and delete components, update dependencies, and improve layout

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,9 @@
-document.querySelector(".hamburger").addEventListener("click", () => {
-  document.querySelector(".nav-links").classList.toggle("expanded");
+const menu = document.querySelector("#menu");
+const openMenuButton = document.querySelector("#open-menu-button");
+
+openMenuButton.addEventListener("click", () => {
+  menu.classList.toggle("w-2/3");
+  menu.classList.toggle("w-0");
+  menu.classList.toggle("-left-full");
+  menu.classList.toggle("left-0");
 });
