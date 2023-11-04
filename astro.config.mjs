69aab17adc63946ae627e288dfbf5d6c3ecf9c7d
Tom Graafmans <graafmans.tom@gmail.com>
2023-11-05 00:35:57 +0100

    Add Preact integration and update compiler options.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://thriving-croquembouche-206c06.netlify.app/",
-  integrations: [tailwind()],
+  integrations: [tailwind(), preact()]
 });
