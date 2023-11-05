f8402a56166f9955daab300426308a48d399a276
Tom Graafmans <graafmans.tom@gmail.com>
2023-11-05 22:50:25 +0100

    Renamed menu to navigation in menu.ts and related components

--- src/assets/js/menu.ts
+++ src/assets/js/menu.ts
@@ -1,15 +1,15 @@
-const menu = document.querySelector("#menu");
-const menuToggle = document.querySelector("#menu-toggle");
-const menuToggleIcon = document.querySelector("#menu-toggle i");
+const navigation = document.querySelector("#navigation");
+const navigationToggle = document.querySelector("#navigation-toggle");
+const navigationToggleIcon = document.querySelector("#navigation-toggle i");
 
-menuToggle.addEventListener("click", () => {
-  menu.classList.toggle("w-2/3");
-  menu.classList.toggle("md:w-1/3");
-  menu.classList.toggle("lg:w-1/4");
-  menu.classList.toggle("w-0");
-  menu.classList.toggle("-left-full");
-  menu.classList.toggle("left-0");
+navigationToggle.addEventListener("click", () => {
+  navigation.classList.toggle("w-2/3");
+  navigation.classList.toggle("md:w-1/3");
+  navigation.classList.toggle("lg:w-1/4");
+  navigation.classList.toggle("w-0");
+  navigation.classList.toggle("-left-full");
+  navigation.classList.toggle("left-0");
 
-  const isChecked = menuToggle.getAttribute("data-checked") === "true";
-  menuToggle.setAttribute("data-checked", String(!isChecked));
+  const isChecked = navigationToggle.getAttribute("data-checked") === "true";
+  navigationToggle.setAttribute("data-checked", String(!isChecked));
 });
