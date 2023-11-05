07916baadac01dc61447541cf5be86fd994189c9
Tom Graafmans <graafmans.tom@gmail.com>
2023-11-05 22:21:27 +0100

    Refactor menu component and rename open menu button to menu toggle

---
+++ src/assets/js/menu.ts
@@ -0,0 +1,13 @@
+const menu = document.querySelector("#menu");
+const menuToggle = document.querySelector("#menu-toggle");
+const menuToggleIcon = document.querySelector("#menu-toggle i");
+
+menuToggle.addEventListener("click", () => {
+  menu.classList.toggle("w-2/3");
+  menu.classList.toggle("w-0");
+  menu.classList.toggle("-left-full");
+  menu.classList.toggle("left-0");
+
+  const isChecked = menuToggle.getAttribute("data-checked") === "true";
+  menuToggle.setAttribute("data-checked", String(!isChecked));
+});
