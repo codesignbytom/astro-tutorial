1433d5edfbe73b7f3f6d8c7addaf8e86d9c4cb2f
Tom Graafmans <graafmans.tom@gmail.com>
2023-11-05 22:46:11 +0100

    Add responsive classes to menu element

--- src/assets/js/menu.ts
+++ src/assets/js/menu.ts
@@ -4,6 +4,8 @@ const menuToggleIcon = document.querySelector("#menu-toggle i");
 
 menuToggle.addEventListener("click", () => {
   menu.classList.toggle("w-2/3");
+  menu.classList.toggle("md:w-1/3");
+  menu.classList.toggle("lg:w-1/4");
   menu.classList.toggle("w-0");
   menu.classList.toggle("-left-full");
   menu.classList.toggle("left-0");
