f1b07751e732b65cab9a3ffa6bab4939c8709cb6
Tom Graafmans <graafmans.tom@gmail.com>
2023-11-05 23:02:54 +0100

    Fix element type errors and add error handling for missing elements

--- src/assets/js/menu.ts
+++ src/assets/js/menu.ts
@@ -1,15 +1,23 @@
-const navigation = document.querySelector("#navigation");
-const navigationToggle = document.querySelector("#navigation-toggle");
-const navigationToggleIcon = document.querySelector("#navigation-toggle i");
+const navigation = document.querySelector("#navigation") as HTMLElement;
+const navigationToggle = document.querySelector(
+  "#navigation-toggle"
+) as HTMLButtonElement;
+const navigationToggleIcon = document.querySelector(
+  "#navigation-toggle i"
+) as HTMLElement;
 
-navigationToggle.addEventListener("click", () => {
-  navigation.classList.toggle("w-2/3");
-  navigation.classList.toggle("md:w-1/3");
-  navigation.classList.toggle("lg:w-1/4");
-  navigation.classList.toggle("w-0");
-  navigation.classList.toggle("-left-full");
-  navigation.classList.toggle("left-0");
+if (navigation && navigationToggle && navigationToggleIcon) {
+  navigationToggle.addEventListener("click", () => {
+    navigation.classList.toggle("w-2/3");
+    navigation.classList.toggle("md:w-1/3");
+    navigation.classList.toggle("lg:w-1/4");
+    navigation.classList.toggle("w-0");
+    navigation.classList.toggle("-left-full");
+    navigation.classList.toggle("left-0");
 
-  const isChecked = navigationToggle.getAttribute("data-checked") === "true";
-  navigationToggle.setAttribute("data-checked", String(!isChecked));
-});
+    const isChecked = navigationToggle.getAttribute("data-checked") === "true";
+    navigationToggle.setAttribute("data-checked", String(!isChecked));
+  });
+} else {
+  console.error("Navigation toggle elements not found");
+}
--- src/assets/js/themeModeChanger.ts
+++ src/assets/js/themeModeChanger.ts
@@ -7,20 +7,30 @@ if (theme === "dark") {
 }
 
 // Theme toggle button
-const themeToggle = document.querySelector("#theme-toggle");
-const themeToggleIconSun = document.querySelector("#theme-toggle-icon-sun");
-const themeToggleIconMoon = document.querySelector("#theme-toggle-icon-moon");
+const themeToggle = document.querySelector(
+  "#theme-toggle"
+) as HTMLButtonElement;
+const themeToggleIconSun = document.querySelector(
+  "#theme-toggle-icon-sun"
+) as HTMLElement;
+const themeToggleIconMoon = document.querySelector(
+  "#theme-toggle-icon-moon"
+) as HTMLElement;
 
-// Switch theme & icon on toggle
-themeToggle.addEventListener("click", () => {
-  document.documentElement.classList.toggle("dark");
-  if (document.documentElement.classList.contains("dark")) {
-    localStorage.setItem("theme", "dark");
-    themeToggleIconSun.classList.remove("hidden");
-    themeToggleIconMoon.classList.add("hidden");
-  } else {
-    localStorage.setItem("theme", "light");
-    themeToggleIconSun.classList.add("hidden");
-    themeToggleIconMoon.classList.remove("hidden");
-  }
-});
+if (themeToggle && themeToggleIconSun && themeToggleIconMoon) {
+  // Switch theme & icon on toggle
+  themeToggle.addEventListener("click", () => {
+    document.documentElement.classList.toggle("dark");
+    if (document.documentElement.classList.contains("dark")) {
+      localStorage.setItem("theme", "dark");
+      themeToggleIconSun.classList.remove("hidden");
+      themeToggleIconMoon.classList.add("hidden");
+    } else {
+      localStorage.setItem("theme", "light");
+      themeToggleIconSun.classList.add("hidden");
+      themeToggleIconMoon.classList.remove("hidden");
+    }
+  });
+} else {
+  console.error("Theme toggle elements not found");
+}
