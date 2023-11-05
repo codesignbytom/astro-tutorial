fc22106e69afd03c3941871f261ca73d7c20bd65
Tom Graafmans <graafmans.tom@gmail.com>
2023-11-05 12:20:52 +0100

    Remove theme toggle and update menu script

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,9 +1,11 @@
 const menu = document.querySelector("#menu");
 const openMenuButton = document.querySelector("#open-menu-button");
 
-openMenuButton.addEventListener("click", () => {
-  menu.classList.toggle("w-2/3");
-  menu.classList.toggle("w-0");
-  menu.classList.toggle("-left-full");
-  menu.classList.toggle("left-0");
+document.addEventListener("astro:page-load", () => {
+  openMenuButton.addEventListener("click", () => {
+    menu.classList.toggle("w-2/3");
+    menu.classList.toggle("w-0");
+    menu.classList.toggle("-left-full");
+    menu.classList.toggle("left-0");
+  });
 });
