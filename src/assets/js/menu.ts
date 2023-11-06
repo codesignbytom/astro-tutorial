2d7cea6775677000d8e768b0f5b63f457cd599f9
Tom Graafmans <graafmans.tom@gmail.com>
2023-11-06 18:22:23 +0100

    Added ViewTransitions to BaseLayout.astro

--- src/assets/js/menu.ts
+++ src/assets/js/menu.ts
@@ -6,18 +6,21 @@ const navigationToggleIcon = document.querySelector(
   "#navigation-toggle i"
 ) as HTMLElement;
 
-if (navigation && navigationToggle && navigationToggleIcon) {
-  navigationToggle.addEventListener("click", () => {
-    navigation.classList.toggle("w-2/3");
-    navigation.classList.toggle("md:w-1/3");
-    navigation.classList.toggle("lg:w-1/4");
-    navigation.classList.toggle("w-0");
-    navigation.classList.toggle("-left-full");
-    navigation.classList.toggle("left-0");
+document.addEventListener("astro:page-load", () => {
+  if (navigation && navigationToggle && navigationToggleIcon) {
+    navigationToggle.addEventListener("click", () => {
+      navigation.classList.toggle("w-2/3");
+      navigation.classList.toggle("md:w-1/3");
+      navigation.classList.toggle("lg:w-1/4");
+      navigation.classList.toggle("w-0");
+      navigation.classList.toggle("-left-full");
+      navigation.classList.toggle("left-0");
 
-    const isChecked = navigationToggle.getAttribute("data-checked") === "true";
-    navigationToggle.setAttribute("data-checked", String(!isChecked));
-  });
-} else {
-  console.error("Navigation toggle elements not found");
-}
+      const isChecked =
+        navigationToggle.getAttribute("data-checked") === "true";
+      navigationToggle.setAttribute("data-checked", String(!isChecked));
+    });
+  } else {
+    console.error("Navigation toggle elements not found");
+  }
+});
